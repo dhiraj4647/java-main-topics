bb1e4d2692461c9c7743f86123f117a9e4734c70
dhiraj4647 <84715353+dhiraj4647@users.noreply.github.com>
2022-05-03 19:14:40 +0530

    Add files via upload

---
+++ IO Streams/Writing Data to File/WritingDataToFile.java
@@ -0,0 +1,20 @@
+/*
+write() method always write data in ASCII character value
+write() does not throws any exception for file, if file is not available then it creates
+the file with that name. 
+FileOutputStream("abc.txt",true) is used for append the data
+*/
+import java.io.*;
+public class WritingDataToFile{
+	public static void main(String args[])throws IOException{
+		File fk = new File("e://abc.txt");
+		FileOutputStream fos = new FileOutputStream(fk,true);
+		fos.write(' ');
+		fos.write('G');
+		fos.write('a');
+		fos.write('r');
+		fos.write('d');
+		fos.write('i');
+		System.out.println("Data written");		
+	}
+}	
